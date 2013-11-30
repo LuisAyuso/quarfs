910476dd5e507eb4e3354ba6e80b6d54e6dadf70
Luis <luis.f.ayuso@gmail.com>
2013-11-30 15:28:13 +0100

    shaders

---
+++ shaders/models.fs.glsl
@@ -0,0 +1,15 @@
+#version 400
+
+// inputs
+in  vec3 color;
+flat in vec4 normal;
+
+// outputs
+out vec4 frag_color;
+
+void main () {
+  vec4 tmp = normalize(normal);
+  vec3 N = tmp.xyz; 
+  //frag_color = vec4 ( (N+color)/2.0, 1.0);
+  frag_color = vec4 ( color, 1.0);
+}
